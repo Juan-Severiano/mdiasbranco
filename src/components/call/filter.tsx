4d7940222d1cc059adb01dcda51be7df837d35fe
David Paz <davizinpaz007@gmail.com>
2024-05-16 21:14:17 -0300

    Filtro de status, prioridade, data concluido em 05/16

--- src/components/call/filter.tsx
+++ src/components/call/filter.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import MenuItem from '@mui/material/MenuItem';
-import { FormControl, Grid, InputAdornment, InputLabel, OutlinedInput, Select } from '@mui/material';
+import { FormControl, Grid, InputAdornment, InputLabel, OutlinedInput, Select, Stack, Typography } from '@mui/material';
 import { CalendarMonth } from '@mui/icons-material';
 import CrisisAlertIcon from '@mui/icons-material/CrisisAlert';
 import Avatar from '@mui/material/Avatar';
 import AvatarGroup from '@mui/material/AvatarGroup';
-import Amarelo from '../../../public/Status/amarelo.jpeg';
-import Azul from '../../../public/Status/azul.jpeg';
-import Ciano from '../../../public/Status/ciano.jpeg';
-import Vermelho from '../../../public/Status/vermelho.jpeg';
+import Amarelo from '../../../public/Status/analisys.jpeg';
+import Azul from '../../../public/Status/pending.jpeg';
+import Ciano from '../../../public/Status/open.jpeg';
+import Vermelho from '../../../public/Status/concluted.jpeg';
 
 const priority = [
   { value: 'Tecnologia', label: 'Tecnologia' },
@@ -39,7 +39,7 @@ export function CallFilters({
 
   const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedDate(event.target.value);
-  };  
+  };
 
   const handlePriorityChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     setSelectedPriority(event.target.value);
@@ -82,11 +82,11 @@ export function CallFilters({
           </FormControl>
         </Grid>
 
-        <Grid item sm={12} md={2}>
+        <Grid item sm={12} md={3}>
           <FormControl fullWidth sx={{ backgroundColor: 'white' }}>
             <InputLabel>Status</InputLabel>
             <Select
-              label='Status'
+              label="Status"
               startAdornment={
                 <AvatarGroup>
                   <Avatar sx={{ width: 13, height: 13 }} src={Vermelho} />
@@ -95,10 +95,18 @@ export function CallFilters({
                   <Avatar sx={{ width: 13, height: 13 }} src={Amarelo} />
                 </AvatarGroup>
               }
-              onChange={handleStatusChange}>
+              onChange={handleStatusChange}
+            >
               {status.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
+                <MenuItem
+                  key={option.value}
+                  value={option.value}
+                >
+                  <Stack justifyContent='space-between' alignItems='center' flexDirection='row' width='100%'>
+                    <Typography fontSize={15}>{option.label}</Typography>
+                    <Avatar sx={{ width: 15, height: 15 }} src={`/Status/${option.value}.jpeg`} />
+                  </Stack>
+
                 </MenuItem>
               ))}
             </Select>
