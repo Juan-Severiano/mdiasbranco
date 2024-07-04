7da7f6a7e92eba6b1ea55505ae46f0b2af3fe922
David Paz <davizinpaz007@gmail.com>
2024-07-04 09:46:43 -0300

    Tela de startup finalizado com sucesso 07/04/24

--- src/pages/[manager]/startup/portfolio.tsx
+++ src/pages/[manager]/startup/portfolio.tsx
@@ -6,7 +6,6 @@ import { Budget } from '../../../components/dashboard/budget';
 import { getDashboardData } from '../../../services/requests/dashboard';
 import { DashData } from '../../../types/problem';
 import coringa from '../../../../public/Status/Coringa.jpg';
-import img1 from '../../../../public/Status/img1.jpeg';
 
 export default function ManagerDashboard() {
   const [dash, setDash] = useState<DashData | null>(null);
@@ -38,9 +37,9 @@ export default function ManagerDashboard() {
               width: '100%',
               padding: '20px',
               borderRadius: '10px',
-              marginBottom: '50px',
+              marginBottom: '70px',
               bgcolor: 'primary.main',
-              backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.3)), url(${img1})`,
+              backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.3)), url(${coringa})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center',
             }}
@@ -53,57 +52,62 @@ export default function ManagerDashboard() {
                 height: 150,
                 left: 20,
                 borderRadius: '50%',
-                marginBottom: '-50px',
+                marginBottom: '-38px',
+                transform: 'translateY(27%)',
               }}
             />
           </Box>
-          <Grid container spacing={2} justifyContent="flex-start" alignItems="center" sx={{ paddingLeft: '20px', paddingRight: '20px', marginBottom: '20px' }}>
-  <Grid item>
-    <Box sx={{ textAlign: 'center' }}>
-      <Typography variant="h4">Nome da Startup</Typography>
-    </Box>
-  </Grid>
-  <Grid item>
-    <Box sx={{ textAlign: 'center' }}>
-      <Typography variant="h6">(Setor)</Typography>
-    </Box>
-  </Grid>
-</Grid>
+          <Grid container spacing={2} justifyContent="flex-start" alignItems="center" sx={{ paddingLeft: '25px', paddingRight: '20px', marginBottom: '20px' }}>
+            <Grid item>
+              <Box sx={{ textAlign: 'center' }}>
+                <Typography variant="h4" sx={{ color: '#003F7D' }}>Nome da Startup</Typography>
+              </Box>
+            </Grid>
+            <Grid item>
+              <Box sx={{ textAlign: 'center', marginTop: '10px'}}>
+                <Typography variant="h6" sx={{ color: '#7A8995' }}>(Setor)</Typography>
+              </Box>
+            </Grid>
+          </Grid>
 
-<Grid container spacing={2} justifyContent="center" alignItems="center" sx={{ paddingLeft: '20px', paddingRight: '20px', marginBottom: '20px' }}>
-  <Grid item xs={12}>
-    <Grid container spacing={2} justifyContent="center" alignItems="center">
-      <Grid item xs={4}>
-        <Box sx={{ padding: '10px', backgroundColor: 'lightgray', borderRadius: '5px', textAlign: 'center', margin: '5px' }}>
-          <Typography variant="body2" fontSize="0.8rem">Razão Social: Nome Razão</Typography>
-        </Box>
-      </Grid>
-      <Grid item xs={4}>
-        <Box sx={{ padding: '10px', backgroundColor: 'lightgray', borderRadius: '5px', textAlign: 'center', margin: '5px' }}>
-          <Typography variant="body2" fontSize="0.8rem">CNPJ: 00.000.000/0000-00</Typography>
-        </Box>
-      </Grid>
-      <Grid item xs={4}>
-        <Box sx={{ padding: '10px', backgroundColor: 'lightgray', borderRadius: '5px', textAlign: 'center', margin: '5px' }}>
-          <Typography variant="body2" fontSize="0.8rem">Área de Atuação: Nome da atuação</Typography>
-        </Box>
-      </Grid>
-    </Grid>
-  </Grid>
-</Grid>
+         
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: '10px',
+              backgroundColor: '#F0F4F8',
+              borderRadius: '10px',
+              margin: '20px 20px',
+              fontSize: '0.9rem',
+              fontWeight: '500',
+              color: '#3F3D56',
+            }}
+          >
+            <Typography variant="body2" sx={{ fontSize: '0.8rem', fontWeight: 'bold', margin: '0 10px',paddingLeft: '10px' }}>
+              Razão Social: <span style={{ fontWeight: 'normal' }}>Nome Razão</span>
+            </Typography>
+            <Typography variant="body2" sx={{ fontSize: '0.8rem', fontWeight: 'bold', margin: '0 10px',  paddingLeft: '60px' }}>
+              CNPJ: <span style={{ fontWeight: 'normal' }}>00.000.000/0000-00</span>
+            </Typography>
+            <Typography variant="body2" sx={{ fontSize: '0.8rem', fontWeight: 'bold', margin: '0 10px',paddingLeft: '30px'  }}>
+              Área de Atuação: <span style={{ fontWeight: 'normal' }}>Nome da atuação</span>
+            </Typography>
+          </Box>
 
-<Grid container spacing={2} sx={{ paddingLeft: '20px', paddingRight: '20px', marginBottom: '20px' }}>
-  <Grid item lg={4} sm={8} xs={12}>
-    <Budget diff={12} trend="up" sx={{ height: '100%', background: "lightgray" }} value="200" />
-  </Grid>
-  <Grid item lg={4} sm={6} xs={12}>
-    <TotalCustomers diff={16} trend="down" sx={{ height: '100%', background: "lightgray" }} value={dash?.countStatusCall.count.received || 0} />
-  </Grid>
-  <Grid item lg={4} sm={6} xs={12}>
-    <TasksProgress sx={{ height: '100%', background: "lightgray" }} diff={22} trend="up" value={dash?.countStatusCall.count.finished || 0} />
-  </Grid>
-</Grid>
 
+          <Grid container spacing={2} sx={{ paddingLeft: '20px', paddingRight: '20px', marginBottom: '20px' }}>
+            <Grid item lg={4} sm={8} xs={12}>
+              <Budget diff={12} trend="up" sx={{ height: '100%', background: "#F0F4F8" }} value="200" />
+            </Grid>
+            <Grid item lg={4} sm={6} xs={12}>
+              <TotalCustomers diff={16} trend="down" sx={{ height: '100%', background: "#F0F4F8" }} value={dash?.countStatusCall.count.received || 0} />
+            </Grid>
+            <Grid item lg={4} sm={6} xs={12}>
+              <TasksProgress sx={{ height: '100%', background: "#F0F4F8" }} diff={22} trend="up" value={dash?.countStatusCall.count.finished || 0} />
+            </Grid>
+          </Grid>
         </Card>
       </Grid>
       <Grid item xs={12} md={3}>
@@ -120,9 +124,12 @@ export default function ManagerDashboard() {
               width: '100%',
             }}
           >
-            <a href="#" style={{ margin: '3px 0', textDecoration: 'none', color: 'blue', background: 'rgba(0, 0, 255, 0.1)', padding: '10px 20px', borderRadius: '5px', fontSize: '16px', width: '100%', textAlign: 'center' }}>Website</a>
-            <a href="#" style={{ margin: '3px 0', textDecoration: 'none', color: 'blue', background: 'rgba(0, 0, 255, 0.1)', padding: '10px 20px', borderRadius: '5px', fontSize: '16px', width: '100%', textAlign: 'center' }}>LinkedIn</a>
-            <a href="#" style={{ margin: '3px 0', textDecoration: 'none', color: 'blue', background: 'rgba(0, 0, 255, 0.1)', padding: '10px 20px', borderRadius: '5px', fontSize: '16px', width: '100%', textAlign: 'center' }}>Instagram</a>
+            <a href="#" style={{ margin: '3px 0', textDecoration: 'none', color: 'blue',  background: "#F0F4F8", padding: '10px 20px', borderRadius: '5px', fontSize: '16px', width: '100%', textAlign: 'center' }}>Website</a>
+
+            <a href="#" style={{ margin: '3px 0', textDecoration: 'none', color: 'blue',  background: "#F0F4F8", padding: '10px 20px', borderRadius: '5px', fontSize: '16px', width: '100%', textAlign: 'center' }}>LinkedIn</a>
+
+            <a href="#" style={{ margin: '3px 0', textDecoration: 'none', color: 'blue',  background: "#F0F4F8", padding: '10px 20px', borderRadius: '5px', fontSize: '16px', width: '100%', textAlign: 'center' }}>Instagram</a>
+            
             <div style={{ width: '100%', height: '1px', backgroundColor: '#ccc', margin: '20px 0' }} />
             <iframe
               title="Mapa"
