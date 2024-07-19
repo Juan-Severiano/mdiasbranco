7df71f384b6d0f190fab697d33e83e98f28212ef
Francisco Juan <juansoussev@gmail.com>
2024-07-19 18:13:55 -0300

    feat: update config file

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-// export const baseURL = 'https://mdiasbranco-api.onrender.com'
-export const baseURL = 'http://localhost'
+export const baseURL = 'https://mdiasbranco-api.onrender.com'
+// export const baseURL = 'http://localhost'
