a7c40d65e88d3b3aecf3c2d82862790b74881674
Francisco Juan <juansoussev@gmail.com>
2024-07-24 08:49:10 -0300

    last commit

--- src/components/dashboard/latest-products.tsx
+++ src/components/dashboard/latest-products.tsx
@@ -28,23 +28,23 @@ export function RankingStartups({ startups = [], sx }: LatestProductsProps): Rea
       <Divider />
       <List>
         {startups.map((startup, index) => (
-          <ListItem divider={index < startups.length - 1} key={startup.position}>
+          <ListItem divider={index < startups.length - 1} key={startup?.position}>
             <Stack flexDirection='row' sx={{ width: '100%' }}>
               <Typography variant="h3" color="primary" fontWeight={600} sx={{ mr: 3 }}>
-                {startup.position} °
+                {startup?.position} °
               </Typography>
               <ListItemAvatar>
                 <Avatar src={`${baseURL}/startup/attachment/${startup?.startup?.attachments?.path}`} />
               </ListItemAvatar>
               <ListItemText
-                primary={startup.startup.name}
+                primary={startup?.startup?.name}
                 primaryTypographyProps={{ variant: 'subtitle1' }}
                 // secondary={`Sessão: ${product.sessao}, ${product.endereco.bairro_distrito}`}
                 secondaryTypographyProps={{ variant: 'body2' }}
               />
               <Tooltip title='Quantidade de chamados finalizados'>
                 <Typography variant="h6" color="primary" fontWeight={600} sx={{ ml: 'auto' }}>
-                  {startup.qtde}
+                  {startup?.qtde}
                 </Typography>
               </Tooltip>
             </Stack>
